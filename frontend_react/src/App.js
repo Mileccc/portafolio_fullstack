fff50c425fa40f3d502552800a69c2c5d6026c42
javi <jaravifa@hotmail.com>
2024-03-03 19:28:41 +0100

    re-exportacion de imagenes

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { About, Header, Footer, Skills, Testimonial, Work } from './container';
 import { Navbar } from './components';
+import './App.scss';
 
 export const App = () => {
   return (
