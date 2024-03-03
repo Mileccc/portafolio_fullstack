6727f2db284d1f408f4715d08915493dadb212c4
javi <jaravifa@hotmail.com>
2024-03-03 16:04:51 +0100

    backend y schemas creados

---
+++ backend_mil/schemaTypes/index.js
@@ -0,0 +1,10 @@
+import testimonials from './testimonials.js'
+import works from './works.js'
+import brands from './brands.js'
+import experiences from './experiences.js'
+import skills from './skills.js'
+import contact from './contact.js'
+import abouts from './abouts.js'
+import workExperience from './workExperience.js'
+
+export const schemaTypes = [testimonials, works, brands, experiences, skills, contact, abouts, workExperience]
