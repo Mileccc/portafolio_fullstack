4101a35fcc8c227ecca1b7cbaaf6036abab5014a
javi <jaravifa@hotmail.com>
2024-03-04 16:58:42 +0100

    Navbar base y scss base

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -4,10 +4,21 @@ import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
         <img src={images.logo} alt='logo' />
       </div>
+
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'skills', 'contact'].map(
+          (item) => (
+            <li className='app__flex p-text' key={`link-${item}`}>
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          )
+        )}
+      </ul>
     </nav>
   );
 };
