5738f84613653b371ee76500060c24f7f9af7b7c
javi <jaravifa@hotmail.com>
2024-03-07 18:28:54 +0100

    componentes de orden superior

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+import { AppWrap } from '../../wrapper';
 import './About.scss';
+import { client, urlFor } from '../../client';
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+      .then((data) => setAbouts(data));
+  }, []);
+
   return (
-    <div>
-      About
-    </div>
+    <>
+      <h2 className='head-text'>I Know that <span>Good Dev</span> <br /> means <span>Good Business</span></h2>
+
+      <div className='app__profiles'>
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.6, type: 'tween' }}
+            className='app__profile-item'
+            key={about.title + index}
+          >
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </>
   );
 };
 
-export default About;
+export default AppWrap(About, 'about');
